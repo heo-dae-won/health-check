c34196e6ca917190ac774ad38f91afc97a793682
heo-dae-won <heodaewon92@kakao.com>
2021-12-25 00:29:22 +0900

    remove testcode

--- src/test/java/com/heo/dae/won/healthcheck/code/CodeServiceTest.java
+++ src/test/java/com/heo/dae/won/healthcheck/code/CodeServiceTest.java
@@ -9,17 +9,17 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 public class CodeServiceTest {
 
-    @Test
-    void 코드길이_8자리_체크(){
-        CodeService codeService = new CodeService();
-        String result = codeService.createNo(8);
-
-        System.out.println("생성된 코드 " + result);
-        assertEquals(result.length(), 8);
-
-        String result2 = codeService.createNo(10);
-
-        System.out.println("생성된 코드 " + result);
-        assertEquals(result2.length(), 10);
-    }
+//    @Test
+//    void 코드길이_8자리_체크(){
+//        CodeService codeService = new CodeService();
+//        String result = codeService.createNo(8);
+//
+//        System.out.println("생성된 코드 " + result);
+//        assertEquals(result.length(), 8);
+//
+//        String result2 = codeService.createNo(10);
+//
+//        System.out.println("생성된 코드 " + result);
+//        assertEquals(result2.length(), 10);
+//    }
 }
